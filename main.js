3fd81050e7b00e903c7cc42d4d096205d4169692
Eduardo Salermo <duduscotta@gmail.com>
2025-11-03 14:42:51 -0300

    sonds commit

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 function preload() {
 	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_gameover = loadSound("gameover.wav");
+	mario_kick = loadSound("kick.wav");
+	mario_die = loadSound("die.wav");
 	setSprites();
 	MarioAnimation();
+	
 }
 
 function setup() {
@@ -12,6 +18,10 @@ function setup() {
 function draw() {
 	game()
 }
+
+
+
+
 
 
 
